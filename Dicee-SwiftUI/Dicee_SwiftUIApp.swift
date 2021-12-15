ea4f7f0151bd5638534d6d1803e3906a4359cd0c
Junsoo Park <matthiaspark0514@gmail.com>
2021-12-15 11:16:44 -0800

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by JUNSOO PARK on 2021-12-15.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
